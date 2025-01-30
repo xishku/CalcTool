d5baee235955dd2496140403781181820654a0c0
paulge <gejianquan@139.com>
2025-01-30 15:09:51 +0800

    build

--- app/dist/runapp.bat
+++ app/dist/runapp.bat
@@ -12,13 +12,18 @@ echo 待转换的文件路径：%input_files_path%
 @REM .\bin\xxx.exe 0 "%input_files_path%" "%cur_path%
 
 set src_file=%input_files_path%\2024条件选股x.xlsx
-set dst_file=%input_files_path%\2024条件选股x.xlsx
+set dst_file=%input_files_path%\2024条件选股x1.xlsx
+set tmp_file=%input_files_path%\2024条件选股x_%timestamp%.xlsx
+
 set sheet_name=条件选股2021
 echo src_file= %src_file%
 echo dst_file= %dst_file%
 echo sheet_name= %sheet_name%
 @REM C:/Users/gejianquan/AppData/Local/Microsoft/WindowsApps/python3.13.exe d:/gejianquan/dev/stock1229/app/app.py "%src_file%" "%dst_file%" "%sheet_name%"
-.\app\app.exe "%src_file%" "%dst_file%" "%sheet_name%
+.\app\app.exe "%src_file%" "%dst_file%" "%sheet_name%" 0
+
+move %src_file% %tmp_file%
+move %dst_file% %src_file%
 
 pause
 
